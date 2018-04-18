d6b6d0e65c5f11d443e72711ad1fd1f18364aad9
BJ0815 <jtt11220@gmail.com>
2018-04-18 14:35:44 +0800

    Q3 solution

--- lib/cart.rb
+++ lib/cart.rb
@@ -14,6 +14,8 @@ class Cart
         total = total + 3 * PRICE * 0.9
       elsif @ordered_items.size == 4
         total = total + 4 * PRICE * 0.85
+      elsif @ordered_items.size == 5
+        total = total + 5 * PRICE * 0.8
       elsif @ordered_items.size == 2
         total = total + 2 * PRICE * 0.95
       else
